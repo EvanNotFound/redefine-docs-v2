6d7881c9963c727fbc5feb201a03940dbf34fc48
Evan Luo <luojiashao@gmail.com>
2023-03-22 20:41:33 -0400

    readme

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,26 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+  <>
+    <img src="https://evan.beee.top/projects/hexo-theme-redefine/statics/logos/logo.webp" style={{width: "40px"}} alt="logo" />
+    <span style={{ marginLeft: '.4em', fontWeight: 700 , fontSize: "1.6rem", color: "#545759"}}>
+      Theme&nbsp;
+    </span>
+    <span style={{ fontWeight: 700 , fontSize: "1.6rem", color: "#545759"}}>
+      R
+    </span>
+    <span style={{ fontWeight: 700 , fontSize: "1.6rem", color: "#A31F34"}}>
+      e
+    </span>
+    <span style={{ fontWeight: 700 , fontSize: "1.6rem", color: "#545759"}}>
+      define
+    </span>
+    <span style={{ fontWeight: 700 , fontSize: "1.6rem", color: "#8A8B8C"}}>
+      &nbsp;Docs
+    </span>
+  </>
+  ),
   project: {
     link: 'https://github.com/EvanNotFound/hexo-theme-redefine',
   },
@@ -17,6 +36,9 @@ const config: DocsThemeConfig = {
   navigation: {
     prev: true,
     next: true
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1
   }
 }
 
