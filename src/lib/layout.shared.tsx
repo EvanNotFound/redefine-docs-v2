e8cabdb038450408f12152b169b71b6877a1bd37
Evan Luo <evan@ohevan.com>
2026-01-17 04:50:42 +0000

    feat: add react-simple-icons package and GitHub icon link
    - add @icons-pack/react-simple-icons dependency at version 13.8.0
    - import and use SiGithub icon for GitHub link in base layout options
    - remove favicon.ico file from src/app directory

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -2,6 +2,7 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 import { i18n } from '@/lib/i18n';
 import redefineIcon from '@/components/icons/redefine.svg';
 import Image from 'next/image';
+import { SiGithub } from '@icons-pack/react-simple-icons';
 
 export function baseOptions(locale: string): BaseLayoutProps {
   
@@ -12,7 +13,7 @@ export function baseOptions(locale: string): BaseLayoutProps {
       <Image src={redefineIcon} alt="Redefine" width={16} height={16} />
       Redefine Docs
       </>,
-      
     },
+    links: [{ type: 'icon', url: 'https://github.com/evannotfound/hexo-theme-redefine', icon: <SiGithub />, text: 'GitHub' }],
   };
 }
