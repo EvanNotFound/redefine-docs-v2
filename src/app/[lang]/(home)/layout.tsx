0b1d5990e7c7023803be3fe4ddf2f668f09851ae
Evan Luo <evan@ohevan.com>
2026-01-17 20:54:11 +0000

    feat(web): add GitHub link to home layout nav
    - add GitHub icon link to home layout navigation

--- src/app/[lang]/(home)/layout.tsx
+++ src/app/[lang]/(home)/layout.tsx
@@ -2,6 +2,8 @@ import type { ReactNode } from 'react';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/lib/layout.shared';
 import { ArrowUpRight } from 'lucide-react';
+import { SiGithub } from '@icons-pack/react-simple-icons';
+
 export default async function Layout({
   params,
   children,
@@ -20,6 +22,12 @@ export default async function Layout({
       {lang === 'zh' ? '演示站点' : 'Demo'}
       <ArrowUpRight className="w-4 h-4 ml-1" />
     </span>,
+  },
+  {
+    type: 'icon',
+    url: 'https://github.com/evannotfound/hexo-theme-redefine',
+    text: 'GitHub',
+    icon: <SiGithub />
   }
   ]}>
     {children}
